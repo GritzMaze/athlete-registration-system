af1f4b269f3e3c93b571487da082ece802f0e7f9
GritzMaze <jivkovvesselin@gmail.com>
2023-12-31 21:00:51 +0200

    setup stripe env variables

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,3 +1,17 @@
-export const config = {
- serverUrl: 'http://localhost:3000/api'
+const stripePublicKey = process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY;
+const stripeRegistrationStripeId =
+  process.env.REACT_APP_STRIPE_REGISTRATION_PRICE_ID;
+
+if (!stripePublicKey) {
+  throw new Error('REACT_APP_STRIPE_PUBLISHABLE_KEY is not set');
 }
+
+if (!stripeRegistrationStripeId) {
+  throw new Error('REACT_APP_STRIPE_REGISTRATION_PRICE_ID is not set');
+}
+
+export const config = {
+  serverUrl: 'http://localhost:3000/api',
+  stripePublicKey,
+  stripeRegistrationStripeId,
+};
