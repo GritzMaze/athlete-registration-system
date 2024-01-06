35971c32b8649d565549497059e7a7fa629705ac
GritzMaze <jivkovvesselin@gmail.com>
2024-01-06 18:14:51 +0200

    Pass the api url as an env variable

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,6 +1,7 @@
 const stripePublicKey = process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY;
 const stripeRegistrationStripeId =
   process.env.REACT_APP_STRIPE_REGISTRATION_PRICE_ID;
+const serverUrl = process.env.REACT_APP_SERVER_URL;
 
 if (!stripePublicKey) {
   throw new Error('REACT_APP_STRIPE_PUBLISHABLE_KEY is not set');
@@ -10,8 +11,12 @@ if (!stripeRegistrationStripeId) {
   throw new Error('REACT_APP_STRIPE_REGISTRATION_PRICE_ID is not set');
 }
 
+if (!serverUrl) {
+  throw new Error('REACT_APP_SERVER_URL is not set');
+}
+
 export const config = {
-  serverUrl: 'http://localhost:3000/api',
+  serverUrl,
   stripePublicKey,
   stripeRegistrationStripeId,
 };
